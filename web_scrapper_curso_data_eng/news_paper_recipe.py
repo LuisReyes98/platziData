b58eba668c301bfed7c2c3654af97bb0ba6b1773
Luis Rogelio Reyes <luisrogelio98@gmail.com>
2020-04-18 18:58:27 -0400

    tokenizing title and body, cleaning data

--- web_scrapper_curso_data_eng/news_paper_recipe.py
+++ web_scrapper_curso_data_eng/news_paper_recipe.py
@@ -2,6 +2,17 @@ import argparse
 import logging
 import ntpath
 from urllib.parse import urlparse
+import hashlib
+
+"""
+nltk es una libreria enorme que se basa en la lectura de
+lenguage natural
+la primera vez que se ejecuta se debaran descargar los modulos necesarios
+"""
+import nltk
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('spanish'))
 
 import pandas as pd
 
@@ -20,10 +31,13 @@ def main(filename):
   newspaper_uid = _extract_newspaper_uid(filename)
 
   df = _add_newspaper_uid_column(df, newspaper_uid)
-
   df = _extract_host(df)
-
   df = _fill_missing_title(df)
+  df = _generate_uids_for_rows(df)
+  df = _remove_new_lines_from_body(df)
+
+  df = _tokenize_column(df, 'title', 'n_tokens_title')
+  df = _tokenize_column(df, 'body', 'n_tokens_body')
 
   return df
 
@@ -75,6 +89,49 @@ def _fill_missing_title(df):
                       .applymap(lambda title_word_list: ' '.join(title_word_list))
                     )
   df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+  return df
+
+
+def _generate_uids_for_rows(df):
+  """ Se generan los uids en base al url """
+  logger.info('Generating uids for each row')
+
+  uids = (df
+          .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+          .apply(lambda hash_object: hash_object.hexdigest())
+          )
+
+  df['uid'] = uids
+
+  return df.set_index('uid')
+
+
+def _remove_new_lines_from_body(df):
+  logger.info('Remove new lines from body')
+
+  stripped_body = (df
+                    .apply(lambda row: row['body'], axis=1)
+                    .apply(lambda body: list(body))
+                    .apply(lambda letters: list(map(lambda letter: letter.replace('\n',' '), letters)))
+                    .apply(lambda letters: ''.join(letters))
+                  )
+  df['body'] = stripped_body
+
+  return df
+
+
+def _tokenize_column(df, column_name, new_column_name):
+  tokenize_column = (df
+          .dropna()
+          .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+          .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+          .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+          .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+          .apply(lambda valid_word_list: len(valid_word_list))
+          )
+
+  df[new_column_name] = tokenize_column
 
   return df
 
